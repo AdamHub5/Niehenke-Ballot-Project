5dbd5a4bcea9ae739594999a018a39df2b009f7a
Adam Niehenke <a.niehenke@setonhill.edu>
2020-04-28 20:42:02 -0400

    Initial Commit

---
+++ Niehenke Ballot Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Niehenke Ballot Project
+//
+//  Created by Adam Niehenke on 4/28/20.
+//  Copyright © 2020 Adam Niehenke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Niehenke Ballot ProjectTests/Niehenke_Ballot_ProjectTests.swift
@@ -0,0 +1,34 @@
+//
+//  Niehenke_Ballot_ProjectTests.swift
+//  Niehenke Ballot ProjectTests
+//
+//  Created by Adam Niehenke on 4/28/20.
+//  Copyright © 2020 Adam Niehenke. All rights reserved.
+//
+
+import XCTest
+@testable import Niehenke_Ballot_Project
+
+class Niehenke_Ballot_ProjectTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
